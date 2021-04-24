050b362a19ef916ba26769a685ebd70ab8d16d74
Javier Lazaro <javier.lazaro@opendeusto.es>
2021-04-24 21:15:14 +0200

    Creado header para metodos con datos

---
+++ ProyectoComunicaciones/datos.h
@@ -0,0 +1,5 @@
+#include<stdio.h>
+#include "comunicaciones.h"
+
+//Guarda una lista de datos en un archivo .csv, nombra el archivo automaticamente en función de los elemntos del array
+int guardaDatos(DATO* listaDatos, int numDatos);
