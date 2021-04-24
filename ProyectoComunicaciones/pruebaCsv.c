4bc3ed2b149feb986aac9770aee38b07adb90d70
Javier Lazaro <javier.lazaro@opendeusto.es>
2021-04-24 21:13:42 +0200

    Añadida funcion para guardar lista de datos a csv

--- ProyectoComunicaciones/pruebaCsv.c
+++ ProyectoComunicaciones/pruebaCsv.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include "csv.h"
+#include "datos.h"
+
 
 void print_buffer(CSV_BUFFER* buffer) {
 
@@ -60,6 +62,48 @@ int cargar_valores(double** x, double** y, CSV_BUFFER* buffer) {
 }
 
 
+int guardaDatos(DATO* listaDatos, int numDatos) {
+
+	char tiempo[20], co2[20], numPer[20], caudal[20];
+	int comienzo = 0;
+	CSV_BUFFER* bufferCreacion = csv_create_buffer();
+	DATO* ptr = listaDatos;
+
+	//El caudal y el numero de personas no cambia en un mismo test así que no hace falta mirarlos cada vez
+	if (snprintf(numPer, sizeof numPer, "%d", listaDatos->numPersonas) < 0) return(1);
+	if (snprintf(caudal, sizeof caudal, "%d", listaDatos->caudal) < 0) return(1);
+
+	char nombreArchivo[100];
+	strcpy_s(nombreArchivo, 100, "..\\Datos\\dat_");
+	strcat_s(nombreArchivo, 100, numPer);
+	strcat_s(nombreArchivo, 100, "_");
+	strcat_s(nombreArchivo, 100, caudal);
+	strcat_s(nombreArchivo, 100, ".csv");
+
+	printf("Guardando datos en la ruta: %s\n", nombreArchivo);
+
+	//Comprobar si existe otro archivo para las mismas condiciones
+	if (csv_load(bufferCreacion, nombreArchivo) == 0) {
+		comienzo = (int)bufferCreacion->rows;
+	}
+
+	for (int i = 0 + comienzo; i < numDatos + comienzo; i++, ptr++)
+	{
+		if(snprintf(tiempo, sizeof tiempo, "%.1f", ptr->tiempo) < 0) return(1);
+		if (snprintf(co2, sizeof co2, "%d", ptr->co2) < 0) return(1);
+		
+		csv_set_field(bufferCreacion, i, 0, tiempo);
+		csv_set_field(bufferCreacion, i, 1, co2);
+		csv_set_field(bufferCreacion, i, 2, numPer);
+		csv_set_field(bufferCreacion, i, 3, caudal);
+	}
+
+	if (csv_save(nombreArchivo, bufferCreacion) == 1) return(1);
+	csv_destroy_buffer(bufferCreacion);
+	return(0);
+}
+
+
 //int main() {
 //
 //	double* x;
@@ -67,7 +111,9 @@ int cargar_valores(double** x, double** y, CSV_BUFFER* buffer) {
 //	int size;
 //
 //	CSV_BUFFER* buffer = csv_create_buffer();
-//	csv_load(buffer, "test.csv");
+//	csv_load(buffer, "..\\Datos\\dat_1_100.csv");
+//
+//	size_t rows = buffer->rows;
 //
 //	size = cargar_valores(&x, &y, buffer);
 //
