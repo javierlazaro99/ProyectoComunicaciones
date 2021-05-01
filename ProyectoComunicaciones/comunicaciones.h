d0d2831c0804e09cd803651dd320aa35550a7b69
Javier Lázaro González <javier.lazaro@opendeusto.es>
2021-05-01 19:51:06 +0200

    Error en definicio de funcion corregido

--- ProyectoComunicaciones/comunicaciones.h
+++ ProyectoComunicaciones/comunicaciones.h
@@ -10,7 +10,7 @@
 int abrirPuerto(HANDLE* port);
 
 //Escribe un mensaje por el puerto serie correspondinte para indicar a la raspberry que comience el experimento
-int comenzarExperimento(HANDLE port);
+int comenzarExperimento(HANDLE port, int time);
 
 //Trata de comunicarse por conexión BT puerto serie con la raspberry
 int leerDatosExperimento(HANDLE port, DATO_LEIDO** listaDatos, int* numDatos);
